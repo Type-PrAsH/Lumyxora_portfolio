0c2d4633fb5103a3f2f2363c49e98c12aea1a4d4
Prashant <prashforwork@gmail.com>
2026-04-20 19:54:22 +0530

    seperated html, css, js

---
+++ tailwind.config.js
@@ -0,0 +1,69 @@
+tailwind.config = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        "on-primary-fixed": "#37003a",
+        "tertiary-fixed-dim": "#ffaaf4",
+        "inverse-on-surface": "#ffebf8",
+        "on-secondary-fixed-variant": "#7f0087",
+        "inverse-primary": "#ffaaf7",
+        error: "#ba1a1a",
+        "on-tertiary": "#ffffff",
+        "on-surface-variant": "#4f4447",
+        "on-tertiary-container": "#a100a2",
+        "on-background": "#380039",
+        "surface-container": "#ffe7f8",
+        tertiary: "#a800a9",
+        "outline-variant": "#d2c3c6",
+        "on-secondary-fixed": "#37003b",
+        "on-primary-fixed-variant": "#702a6f",
+        "primary-fixed": "#ffd6f7",
+        "surface-container-high": "#ffdff7",
+        "tertiary-fixed": "#ffd7f5",
+        "on-surface": "#380039",
+        "on-tertiary-fixed-variant": "#810081",
+        "surface-container-highest": "#ffd6f7",
+        "on-primary": "#ffffff",
+        "on-primary-container": "#863e85",
+        "surface-container-low": "#ffeff9",
+        "secondary-container": "#f759fe",
+        "tertiary-container": "#ffbef4",
+        "on-secondary": "#ffffff",
+        "surface-variant": "#ffd6f7",
+        "surface-tint": "#8b4289",
+        "on-tertiary-fixed": "#380038",
+        "surface-bright": "#fff7f9",
+        "on-error-container": "#93000a",
+        surface: "#fff7f9",
+        "inverse-surface": "#551156",
+        background: "#fff7f9",
+        "primary-container": "#ffbef7",
+        "surface-dim": "#ffcaf6",
+        "error-container": "#ffdad6",
+        outline: "#817477",
+        "secondary-fixed": "#ffd6f8",
+        "on-secondary-container": "#620068",
+        "on-error": "#ffffff",
+        secondary: "#a600b0",
+        "secondary-fixed-dim": "#ffa9fa",
+        "surface-container-lowest": "#ffffff",
+        "primary-fixed-dim": "#ffaaf7",
+        primary: "#8b4289"
+      },
+      borderRadius: {
+        DEFAULT: "0.25rem",
+        lg: "0.5rem",
+        xl: "0.75rem",
+        full: "9999px"
+      },
+      spacing: {},
+      fontFamily: {
+        headline: ["Epilogue"],
+        body: ["Manrope"],
+        label: ["Manrope"],
+        display: "Epilogue"
+      }
+    }
+  }
+};
